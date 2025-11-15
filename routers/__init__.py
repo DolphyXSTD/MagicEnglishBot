96ebca29c85dcc2b84e3e5be52c639e5ad4aecc2
Dolphy <timofey31052008@gmail.com>
2025-11-15 23:20:09 +0300

    Little fix

--- routers/__init__.py
+++ routers/__init__.py
@@ -5,4 +5,3 @@ from .commands import router as commands_router
 router = Router(name=__name__)
 
 router.include_router(commands_router)
-
--- routers/commands/admin.py
+++ routers/commands/admin.py
@@ -18,12 +18,10 @@ async def send_post(message : Message, state : FSMContext):
     try:
         for user in database.get_all_users():
             await message.bot.copy_message(
-                chat_id=user,
+                chat_id=user[0],
                 from_chat_id=message.chat.id,
                 message_id=message.message_id
             )
         await state.clear()
     except Exception as e:
         await message.answer(f'Сообщение не удалось разослать, попробуйте еще раз: {e}')
-
-
